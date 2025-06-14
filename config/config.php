633f33f2213e5c42328e5c4f4048fd8d227db938
Adrian <ionicheadrian@yahoo.com>
2025-06-15 00:51:13 +0300

    am uitat un ' ; '😐

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 //pornim sesiunea 
 session_start();
 //ne conectam la db
-require_once 'dbconnection.php'
-//functiile helper
+require_once 'dbconnection.php';
+// functiile helper
 require_once 'functions.php';
 ?>
